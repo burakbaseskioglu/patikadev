4a0ce31c7b2c80e2cf8f76eec627aa1790f8a52e
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-06-24 20:19:05 +0300

    while ve foreach dongüleri calisma notlari

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******** while ********");
+
+            //1'den başlayarak console'dan girilen sayıya kadar ortalama hesaplayan program
+            int toplam = 0;
+            int sayac = 1;
+            Console.Write("Sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+
+            while (sayi >= sayac)
+            {
+                toplam = toplam + sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            //'a'dan 'z'ye kadar olan tüm harfleri yazdıran program
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+                Console.WriteLine(karakter);
+                karakter++;
+            }
+
+            Console.WriteLine("******** foreach ********");
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
