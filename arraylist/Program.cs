642c5845ff6136c4fd2085994760cbba3a9b777c
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-07-06 14:13:34 +0300

    ArrayList calisma notlari

---
+++ arraylist/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            // arrayList.Add("Ayşe");
+            // arrayList.Add(2);
+            // arrayList.Add(true);
+            // arrayList.Add('a');
+
+            //İçerisindeki verilere erişme
+            //Console.WriteLine(arrayList[0]);
+
+            // foreach (var item in arrayList)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            //AddRange()
+            Console.WriteLine("******** AddRange() ********");
+            //string[] renkler = { "Kırmızı", "Sarı", "Yeşil" };
+            List<int> sayilar = new List<int>() { 1, 8, 3, 7, 9, 91, 5 };
+            //arrayList.AddRange(renkler);
+            arrayList.AddRange(sayilar);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort()
+            Console.WriteLine("******** Sort() ********");
+            arrayList.Sort();
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //BinarySearch()
+            Console.WriteLine("******** BinarySearch() ********");
+            //BinarySearch'ü kullanabilmek için listeyi önce sıralayıp sonra index'e ulaşabiliriz.
+            Console.WriteLine(arrayList.BinarySearch(9));
+
+            //Reverse()
+            Console.WriteLine("******** Reverse() ********");
+            arrayList.Reverse();
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Clear()
+            Console.WriteLine("******** Reverse() ********");
+            arrayList.Clear();
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
