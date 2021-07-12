9de0e3345558e70f73daaf1376d221db5d182a91
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-07-12 23:51:20 +0300

    sinif-kavrami calisma notlari

---
+++ sinif-kavrami/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 23425634;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Arda";
+            calisan2.No = 255646789;
+            calisan2.Departman = "Satın Alma";
+
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine($"Çalışanın Adı:{Ad}");
+            Console.WriteLine($"Çalışanın Soyadı:{Soyad}");
+            Console.WriteLine($"Çalışanın Numarası:{No}");
+            Console.WriteLine($"Çalışanın Departmanı:{Departman}");
+        }
+    }
+}
