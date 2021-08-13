938be6f17271326233be52f5150bc0f4d0268821
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-13 20:56:08 +0300

    Klasor tasindi

---
+++ CSharpProjeleri/KolaySeviye/Algoritma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Virgüllü ifadeyi giriniz:");
+            string str = Console.ReadLine();
+            Console.WriteLine(DeleteCharacter(str));
+        }
+
+        static string DeleteCharacter(string expression)
+        {
+            string[] strArr = expression.Split(',');
+            int index = int.Parse(strArr[1].Trim());
+            return strArr[0].Remove(index, 1);
+        }
+    }
+}
