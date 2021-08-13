1774b143ba74683ac590b74f4263973b1ff0e4e4
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-13 18:30:44 +0300

    C# 101 projeleri 1. ve 2. ödev eklendi

---
+++ CSharpProjeleri/KolaySeviye/UcgenCizme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgen kaç katlı olsun? :");
+            int floorNumber = int.Parse(Console.ReadLine());
+
+            DrawTriangle(floorNumber);
+        }
+        static void DrawTriangle(int floorNumber)
+        {
+            for (int i = 1; i <= floorNumber; i++)
+            {
+                for (int j = i; j < floorNumber; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
