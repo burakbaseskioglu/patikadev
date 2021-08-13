4d9ad122c74604bc3f2803354e82166d42efdfd3
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-13 22:25:25 +0300

    Karakter tersten yazdırma ödevi eklendi

---
+++ CSharpProjeleri/KolaySeviye/TerstenKarakterYazdirma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TerstenKarakterYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İfade giriniz:");
+            string expression = Console.ReadLine();
+
+            Console.WriteLine(WriteFirstCharacterLast(expression));
+        }
+
+        static string WriteFirstCharacterLast(string expression)
+        {
+            char firstCharacter = expression[0];
+            return expression.Substring(1, expression.Length - 1) + firstCharacter;
+        }
+    }
+}
