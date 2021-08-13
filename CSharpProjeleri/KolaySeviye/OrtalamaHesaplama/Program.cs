7e4d365b87be2212447261629eea566f964393c2
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-13 18:33:09 +0300

    C# 101 Projeleri 1. ödev eklendi

---
+++ CSharpProjeleri/KolaySeviye/OrtalamaHesaplama/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OrtalamaHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            double toplam = 0;
+            Console.Write("Fibonacci dizisi kaç elemanlı olsun? :");
+            int boyut = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < boyut; i++)
+            {   
+                toplam = toplam + Toplam(sayi);
+                //Console.WriteLine(Toplam(sayi));
+                sayi++;
+            }
+            Console.WriteLine($"Fibonacci dizisinin ortalaması {toplam / boyut}");
+        }
+
+        static int Toplam(int sayi)
+        {
+            if (sayi == 0)
+            {
+                return 0;
+            }
+            else if (sayi == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Toplam(sayi - 1) + Toplam(sayi - 2);
+            }
+        }
+    }
+}
