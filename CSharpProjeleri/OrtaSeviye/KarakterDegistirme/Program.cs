3c7894de2adf2f9f0923fd47d43a606d2f3bfaba
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-15 23:13:57 +0300

    Karakter Degistirme ödevi eklendi

---
+++ CSharpProjeleri/OrtaSeviye/KarakterDegistirme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kelimeyi giriniz:");
+            string word = Console.ReadLine();
+            string cutWord = word.Substring(1, word.Length - 2);
+            Console.WriteLine(word[word.Length - 1] + cutWord + word[0]);
+        }
+    }
+}
