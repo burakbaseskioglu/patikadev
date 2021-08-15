2af9a8d8446166afa33f123071d9e800b59eacd2
Burak Baseskioglu <burakbaseskioglu@hotmail.com>
2021-08-15 17:27:53 +0300

    MutlakKareAlma ödevi eklendi

---
+++ CSharpProjeleri/OrtaSeviye/MutlakKareAlma/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MutlakKareAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dizi boyutunu giriniz:");
+            int arrLength = int.Parse(Console.ReadLine());
+
+            List<int> kucukOlanlar = new List<int>();
+            List<int> buyukOlanlar = new List<int>();
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write("Sayı giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+
+                if (sayi < 67)
+                {
+                    kucukOlanlar.Add(sayi);
+                }
+                else if (sayi > 67)
+                {
+                    buyukOlanlar.Add(sayi);
+                }
+            }
+            CalcLessThan(kucukOlanlar);
+            CalcMoreThan(buyukOlanlar);
+        }
+
+        static void CalcLessThan(List<int> numberList)
+        {
+            int result = 0;
+            foreach (var item in numberList)
+            {
+                result = result + (67 - item);
+            }
+            Console.WriteLine($"67 küçük olan sayıların farklarının toplamı={result}");
+        }
+
+        static void CalcMoreThan(List<int> numberList)
+        {
+            int result = 0;
+            foreach (var item in numberList)
+            {
+                result = result + (int)Math.Pow((item - 67), 2);
+            }
+            Console.WriteLine($"67'den büyük sayıların farklarının karesi={result}");
+        }
+    }
+}
